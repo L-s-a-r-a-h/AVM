def2cb2bb23338666c374da2648626760c812fd6
unknown <tpx3924@autuni.ac.nz>
2023-07-04 18:31:59 +1200

    create app

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import MenuBar from './component/MenuBar';
+
+function App() {
+  return (
+    <MenuBar/>
+  );
+}
+
+export default App;
---
+++ my-app/src/component/MenuBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+import "./MenuBar.css";
+
+import Home from './Home'
+function MenuBar() {
+    return (
+        <Router>
+            <div class="leftNav">
+                <Navbar bg="light" expand="lg">
+                    <Container className="container" fluid>
+
+                <div class="head">
+
+                 <h2>Automated Vulnerability Management </h2>
+
+                 </div>
+                      
+                      <div class = "menubar">
+                        <Navbar.Brand href="/Home"><h4 className="home">Home</h4></Navbar.Brand>
+                        <Nav.Link as={Link} to={"/start"}><h4 className="linkText">Start </h4></Nav.Link>
+                        <Nav.Link as={Link} to={"/settings"}><h4 className="linkText">Settings </h4></Nav.Link>
+
+                        <h4>log out</h4>
+                        </div>
+                    </Container>
+                </Navbar>
+            </div>
+
+
+            <Routes>
+                <Route exact path='/' element ={<Home/>}/>
+                <Route path='Home' element={<Home/>}> </Route>
+
+            </Routes>
+
+        </Router>
+    );
+}
+
+export default MenuBar;
