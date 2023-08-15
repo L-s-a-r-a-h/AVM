2ec1aa9cebde81f3339b172f0fa77a9202f8e02b
jamjamin <benjamjamin02@gmail.com>
2023-08-15 22:38:30 +1200

    CVE Data now shows on Output Page (and sorted)

--- my-app/src/component/Output.js
+++ my-app/src/component/Output.js
@@ -1,40 +1,17 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import MenuBar from "./MenuBar";
+import { useLocation } from "react-router-dom"
+
+
 function Output() {
-    const test_cves = [
-        {
-            priorty: 1,
-            cve_id: "CVE-123-321",
-            severity: "CRITICAL",
-            name: "MS Internet Explorer Unsupported Version Details",
-            ip: "192.168.1.1"
-        },
-        {
-            priorty: 2,
-            cve_id: "CVE-246-642",
-            severity: "HIGH",
-            name: "Microsoft Paint 3D Code Execution",
-            ip: "192.168.1.1"
-        },
-        {
-            priorty: 3,
-            cve_id: "CVE-369-963",
-            severity: "MEDIUM",
-            name: "SMB Signing Not Required",
-            ip: "192.168.1.1"
-        },
-        {
-            priorty: 4,
-            cve_id: "CVE-434-235",
-            severity: "MEDIUM",
-            name: "Microsoft OS Outdated",
-            ip: "192.168.1.2"
-        }
-    ]
-    
-    const [CVEItems, setItems] = useState(test_cves);
+    // Get CVE Report from Start Page
+    const location = useLocation();
+    const cve_data = location.state;
+
+    const [CVEItems, setItems] = useState([]);
     const [selected, setSelected] = useState(null);
-   
+    
+    /*
     function addCVE(priorty, cve_id, severity, name, ip) {
         setItems([
             ...CVEItems,
@@ -45,8 +22,28 @@ function Output() {
                 name: name,
                 ip: ip
             }
-        ])
-    }
+            ]   
+        )
+    }//*/
+
+    // Process Input CVE Data
+    useEffect(() => {
+        let cve_list = [];
+        for (let data of cve_data) {
+            cve_list.push({
+                cve_id: data.CVE,
+                severity: data.Risk,
+                name: data.Name,
+                ip: data.Host,
+                Criticality_Score: parseFloat(data.CVSS),
+                description : data.Description
+            })
+        }
+        cve_list.sort((a,b) => {
+            return  b.Criticality_Score - a.Criticality_Score;
+        })
+        setItems(cve_list);
+    }, []);
 
     const showCVE = (i) => {
         if (selected == i) {
@@ -55,7 +52,6 @@ function Output() {
 
         setSelected(i);
     }
-
 
     return (
         <div> 
@@ -75,29 +71,18 @@ function Output() {
                         return (
                             <div class="cve-accordion">
                                 <li key={CVEItem.cve_id} class="cve-list-item" onClick={() => showCVE(i)}>
-                                    <div class="cve-col col-1"> {CVEItem.priorty} </div>
+                                    <div class="cve-col col-1"> {CVEItems.indexOf(CVEItem) + 1} </div>   
                                     <div class="cve-col col-2"> {CVEItem.cve_id} </div>
-                                    <div class="cve-col col-3"> {CVEItem.severity} </div>
+                                    <div class="cve-col col-3"> {CVEItem.severity === "Critical" ? "CRITICAL" : CVEItem.severity} </div>
                                     <div class="cve-col col-4"> {CVEItem.name} </div>
                                     <div class="cve-col col-5"> {CVEItem.ip} </div>
                                 </li>
                                 <div class={
-                                        selected === i ? 'cve-content-box show' : 'cve-content-box'}
-                                >
+                                    selected === i ? 'cve-content-box show' : 'cve-content-box'
+                                }>
                                     <div class="cve-content">
                                         <h4> Description </h4>  
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
-                                        sed do eiusmod tempor 
-                                        incididunt ut labore et dolore magna aliqua. 
-                                        Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                                        laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-                                        <h4> Risks </h4>  
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
-                                        sed do eiusmod tempor 
-                                        incididunt ut labore et dolore magna aliqua. 
-                                        Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                                        laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                        <p>{CVEItem.description}</p>
 
                                         <h4> Solution </h4>  
                                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
@@ -115,7 +100,6 @@ function Output() {
         </div>
         </div>
     );
-
 }
 
 export default Output;
