4f1c88365896cd2a930845411857e4d4118feee0
chrjssy <christiangamble2002@gmail.com>
2023-09-25 19:07:30 +1300

    Updates to the login page
    Can login with test username and password. Incorrect or missing details are announced.

--- my-app/src/component/Login.js
+++ my-app/src/component/Login.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { useForm } from "react-hook-form";
+import React from 'react';
+
 import { useNavigate } from 'react-router-dom';
 import { useState } from "react";
 import "./styles/Login.css";
@@ -9,23 +9,27 @@ export default function Login() {
     function signinBtn(){
         let signupBtn = document.getElementById("signupBtn");
         let signinBtn = document.getElementById("signinBtn");
+        let submitBtn = document.getElementById("submitBtn");
         let nameField = document.getElementById("nameField");
         let title = document.getElementById("title");
         nameField.style.maxHeight = "0";
         title.innerHTML = "Sign In";
         signupBtn.classList.add("disable");
         signinBtn.classList.remove("disable");
+        submitBtn.classList.remove("disable");
     }
 
     function signupBtn (){
         let signupBtn = document.getElementById("signupBtn");
         let signinBtn = document.getElementById("signinBtn");
+        let submitBtn = document.getElementById("submitBtn");
         let nameField = document.getElementById("nameField");
         let title = document.getElementById("title");
         nameField.style.maxHeight = "60px";
         title.innerHTML = "Sign Up";
         signupBtn.classList.remove("disable");
         signinBtn.classList.add("disable");
+        submitBtn.classList.add("disable");
     }
 
     const navigate = useNavigate();
@@ -33,44 +37,40 @@ export default function Login() {
     const [email, setEmail] = useState("");
 
     const [password, setPassword] = useState("");
-    const [user, setUser] = useState()
+   
 
     const [message, setMessage] = useState("");
+    const [showSubmitButton, setShowSubmitButton] = useState(false); // Track if Submit button should be displayed
 
-    function validateForm() {
-
-        return email.length > 0 && password.length > 0;
-
-    }
+  
     //test login
-    const users = [{ username: "aaa", password: "bbb" }];
 
     const handleSubmit = async event => {
 
-        event.preventDefault();
-        // ---------------authenticate user log in from server ---------------------------
-        //const user = { email, password };
-        // const url = ""; // server url need to create later
-        // const response = await fetch(url,user);
-        // setUser(response.data);
-        // store the user in localStorage
-
-        // localStorage.setItem('user', response.data);
-
-        const account = users.find((user) => user.username === email);
-
-        if (account && account.password === password) {
-            localStorage.setItem("authenticated", true);
-            navigate("/Home");
-        } else {
-            setMessage("Incorrect Login details.Try again. or create account");
+      
+        // Check if both email and password are provided
+        if (!email || !password) {
+            setMessage('Please enter both email and password.');
+            return; // Stop login attempt if fields are not filled
         }
 
+        // Simulate authentication logic
+        const testUsername = 'aaa';
+        const testPassword = 'bbb';
 
-    }
-    if (user) {
-        return <div>{user.name} is loggged in</div>;
-    }
+        if (email === testUsername && password === testPassword) {
+            localStorage.setItem('authenticated', true);
+            navigate('/Home');
+        } else {
+            setMessage('Incorrect Login details. Try again or create an account.');
+        }
+    };
+
+    const toggleSubmitButton = () => {
+        setShowSubmitButton(true);
+    };
+
+
     return (
         <div>
             <div class="container">
@@ -80,24 +80,44 @@ export default function Login() {
                     <div class="input-group">
                         <div class="input-field" id="nameField">
                             <i class="fa-solid fa-user"></i>
-                            <input type="text" placeholder="Name" name="fullName" required />
+                            <input 
+                                type="text" 
+                                placeholder="Name" 
+                                name="fullName" required />
                         </div>
 
                         <div class="input-field">
                             <i class="fa-solid fa-envelope"></i>
-                            <input type="email" placeholder="Email" name="email" required />
+                            <input 
+                                type="email" 
+                                placeholder="Email" 
+                                name="email" 
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required />
                         </div>
 
                         <div class="input-field">
                             <i class="fa-solid fa-lock"></i>
-                            <input type="password" placeholder="Password" name="password" required />
+                            <input 
+                                type="password" 
+                                placeholder="Password" 
+                                name="password" 
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)} 
+                                required />
                         </div>
                         <p>Forgot password? <a href="#"> Click Here</a></p>
                     </div>
                     <div class="btn-field">
                         <button type="button" id="signupBtn" onClick={signupBtn}>Sign Up</button>
-                        <button type="button" id="signinBtn" class="disable" onClick={signinBtn}>Sign In</button>
+                        <button type="button" id="signinBtn" class="disable" onClick={() => { signinBtn(); toggleSubmitButton(); }}>Sign In</button>
+                        
                     </div>
+                    <div class="subbtn-field">
+                        <button type="button" id="submitBtn" onClick={handleSubmit}>Submit</button>
+                    </div>
+                    {message && <p className="error-message">{message}</p>}
                 </form>
             </div>
         </div>
