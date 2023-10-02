c6ec6333e17c191fe01b578f8e0955cc259cd218
simonquach56 <simonquach56@gmail.com>
2023-10-02 14:29:07 +1300

    Adding Icons

--- my-app/src/component/Home.js
+++ my-app/src/component/Home.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./styles/HomeBox.css";
 import MenuBar from "./MenuBar";
+import { BsFiletypeCsv } from "react-icons/bs";
+import { AiFillFolderOpen } from "react-icons/ai";
+
 
 const Home = () => {
     return (
@@ -9,8 +12,14 @@ const Home = () => {
             <MenuBar />
             <div className="main-content">
                 <div className="home-boxes">
-                    <Link to="/start" className="home-box">Upload Vulnerability Report</Link>
-                    <Link to="/Output" className="home-box">View Previous Results</Link>
+                    <Link to="/start" className="home-box">
+                        <BsFiletypeCsv/>
+                        <div className="box-content">Upload Vulnerability Report</div>
+                    </Link>
+                    <Link to="/Output" className="home-box">
+                        <AiFillFolderOpen/>
+                        <div className="box-content">View Previous Results</div>
+                    </Link>
                 </div>
             </div>
         </div>
