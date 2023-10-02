25c7734794d35583b8648ef63b5d31bbd5579f3f
L-s-a-r-a-h <tpx3924@autuni.ac.nz>
2023-10-02 15:15:39 +1300

    update form

--- my-app/src/component/FormComponents/Form.js
+++ my-app/src/component/FormComponents/Form.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
-import FormSystems from './FormSystems';
-import FormPriority from './FormPriority';
-import FormApplications from './FormApplications';
+
+import React, { Component } from "react";
+import { useForm } from "react-hook-form";
+import FormSystems from "./FormSystems";
+import FormApplications from "./FormApplications";
+import FormPriority from "./FormPriority";
+import { useLocation } from "react-router-dom"
+
+import Output from "../Output";
+
 
 export class Form extends Component {
+  
   state = {
     page: 1,
     selectedSystems: [], // To store selected systems
     systemName: '',
+    apps: this.props
   };
 
   // Function to add a selected system
@@ -32,61 +40,67 @@ export class Form extends Component {
   };
 
   nextStep = () => {
-    const { page: step } = this.state;
+    const { page } = this.state;
     this.setState({
-      page: step + 1,
+      page: page + 1,
     });
   };
 
   prevStep = () => {
-    const { page: step } = this.state;
+    const { page } = this.state;
     this.setState({
-      page: step - 1,
+      page: page - 1,
     });
   };
-
-  handleChange = (input) => (e) => {
+  handleChange = input => e => {
     this.setState({ [input]: e.target.value });
   };
+  updateScore = (scores)=>{
+  this.setState({ selectedSystems: scores})
+  console.log(this.props);
+ 
+}
 
+  setSystems = (data) => {
+   this.setState({systems:data})
+  }
   render() {
-    const { page: step, selectedSystems, systemName } = this.state;
-    const values = { systemName };
+    const { page: step, selectedSystems, systemName ,apps} = this.state;
+    const values = {systemName};
 
     switch (step) {
-      case 1:
-        return (
-          <FormSystems
-            nextStep={this.nextStep}
-            handleChange={this.handleChange}
-            values={values}
-            selectedSystems={selectedSystems}
-            addSelectedSystem={this.addSelectedSystem}
-            removeSelectedSystem={this.removeSelectedSystem}
-            updateSelectedSystems={this.updateSelectedSystems}
-          />
-        );
+      case 1: return ( <FormSystems
+        nextStep={this.nextStep}
+        handleChange={this.handleChange}
+        values={values}
+        selectedSystems={selectedSystems}
+        addSelectedSystem={this.addSelectedSystem}
+        removeSelectedSystem={this.removeSelectedSystem}
+        updateSelectedSystems={this.updateSelectedSystems}
+      />
+      );
       case 2:
-        return (
-          <FormPriority
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-            handleChange={this.handleChange}
-            values={values}
-            selectedSystems={selectedSystems}
-          />
-        );
-      case 3:
-        return (
-          <FormApplications
-            nextStep={this.nextStep}
-            prevStep={this.prevStep}
-            handleChange={this.handleChange}
-            values={values}
-          />
-        );
-      default:
-        return <p>Default content</p>;
+        return (          <FormPriority
+          nextStep={this.nextStep}
+          prevStep={this.prevStep}
+          handleChange={this.handleChange}
+          values={values}
+          selectedSystems={selectedSystems}
+          updateScore={this.updateScore}
+        />
+      );
+    case 3:
+      return (
+        <FormApplications
+          nextStep={this.nextStep}
+          prevStep={this.prevStep}
+          handleChange={this.handleChange}
+          values={values}
+          apps={apps}
+        />
+      );
+    default:
+      return <p>Default content</p>;
     }
   }
 }
--- my-app/src/component/FormComponents/FormApplications.js
+++ my-app/src/component/FormComponents/FormApplications.js
@@ -1,12 +1,13 @@
-import useFormContext from "../../hooks/useFormContext"
 import React, { Component } from 'react';
 
 // form page for mapping the applications to the mian systems
 export class FormApplications extends Component {
 
-    
-    continue = e => {
+
+  continue = e => {
     e.preventDefault();
+    //-----------------------------------------------------//
+    //process form
     this.props.nextStep();
   };
   back = e => {
@@ -14,28 +15,67 @@ export class FormApplications extends Component {
     this.props.prevStep();
 
   }
-  
-  render() {
-    const { values, handleChange } = this.props;
+  add = ()=>{
 
-    return(
-<>
-
-<p>Please select the applications used in each system</p>
-<button type="button"  onClick={this.continue}>next 
-        </button>
-        
-       < button type="button"  onClick={this.back}>back 
-        </button>
-</>
-       
-        
-    )
- 
-   
-    
+  console.log()
   }
-    
-    
+  render() {
+    const { selectedSystems, handleChange, values,apps } = this.props;
+    console.log(this.props.apps)
+    return (
+      <div className="form-container">
+
+
+        <p>Please select the applications used in each system</p>
+        <form id="form">
+          {/* {selectedSystems && selectedSystems.length > 0 ? (
+            selectedSystems.map((system, index) => (
+              <div key={index} className="system-priority">
+                <label className="system-label">{system}</label>
+                <select
+                  className="system-select"
+                  name={`${system}`}
+                  value={values[`${system}-${index}`]}
+                >
+                  <option value="1">1 (Lowest)</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5 (Highest)</option>
+                </select>
+              </div>
+            ))
+          ) : (
+            <p>No selected systems to display.</p>
+          )} */}
+               <button
+          type="button"
+          onClick={this.add}
+          className="button"
+        >
+          Add
+        </button>
+        </form>
+
+        <div className="button-container">
+          <></>
+          <button type="button" className="button back-button" onClick={this.back}>
+            Back
+          </button>
+          <button type="button" className="button" onClick={this.continue}>
+            Next
+          </button>
+
+        </div>
+      </div>
+
+
+    )
+
+
+
+  }
+
+
 }
 export default FormApplications
